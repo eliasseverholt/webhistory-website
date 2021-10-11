1948f681d70beb2b5cd1ce0af8b071f2567fb97b
Severholt Fourati Elias <elias.severholtfourati@edu.nacka.se>
2021-10-11 09:18:30 +0200

    added menu-element

--- scripts/menuElement.js
+++ scripts/menuElement.js
@@ -1,41 +1,43 @@
 class MenuElement extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `<div class="container-fluid">
-        <nav class="navbar navbar-expand-lg navbar-light bg-white">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="../index.html">Webbhistoria</a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="../index.html"
-                    >Timeline</a
-                  >
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="../impact.html">Impact</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="../security.html">Security</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="../privacy.html">Privacy</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </div>`
+    this.innerHTML = `      <nav class="navbar navbar-expand-sm navbar-light bg-light px-sm-4">
+        <a class="navbar-brand" href="/index.html#">Webbhistory</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link ${
+                this.getAttribute("active") === "Timeline"
+                  ? "active"
+                  : undefined
+              }" href="/index.html#">Timeline</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link ${
+                this.getAttribute("active") === "Impact" ? "active" : undefined
+              }" href="/impact.html">Impact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link ${
+                this.getAttribute("active") === "Security"
+                  ? "active"
+                  : undefined
+              }" href="/security.html">Security & Privacy</a>
+            </li>
+          </ul>
+        </div>
+      </nav>`
   }
 }
 
